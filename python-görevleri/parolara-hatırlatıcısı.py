6cb0c014eefad48df7d2b73cd9fd1434b5831bd4
Alperen <alperenalbayin@yaani.com>
2021-07-16 11:27:55 -0400

    parola hatırlatıcısı eklendi

---
+++ python-görevleri/parolara-hatırlatıcısı.py
@@ -0,0 +1,5 @@
+isim = input("lütfen kullanıcı adınızı giriniz").strip().title()
+if isim == "Colonel" :
+  print("Merhaba, Joseph! Şifre:W@12")
+else :
+  print("Merhaba, {}! Sonra görüşürüz.".format(isim))
