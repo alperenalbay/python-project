b8a21f742b9f377b900a05b9c3bfcd0f8d6a4ba5
alperen <85994779+alperenalbay@users.noreply.github.com>
2021-08-04 11:16:56 +0300

    Create def_fibonacci.py

---
+++ python-görevleri/def_fibonacci.py
@@ -0,0 +1,10 @@
+def  forfibonacci(sinir):
+    sayi1=0
+    sayi2=1
+    print(sayi1)
+    print(sayi2)
+    for i in range(sinir-2):
+        sayi3=sayi1+sayi2
+        print(sayi3)
+        sayi1=sayi2
+        sayi2=sayi3
